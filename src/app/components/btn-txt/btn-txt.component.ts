15d22b5b159366cc5d23e35e9eb2ca01110ca284
MkshSahani <msco18333@gmail.com>
2023-01-19 23:01:09 +0530

    front-end-change iteration-1

---
+++ src/app/components/btn-txt/btn-txt.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+@Component({
+    selector: 'btn-txt',
+    templateUrl: './btn-txt.component.html',
+    styleUrls: ['./btn-txt.component.css']
+})
+export class BtnTextComponent implements OnInit {
+    
+    ngOnInit(): void {
+        
+    }
+}
